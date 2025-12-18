9fb9281645216f50de5d35f316e46ae911172dba
zhan7236 <zhan7236@users.noreply.github.com>
2025-12-18 12:06:09 +0000

    deploy: 887b4499e29b187a75d0978d719144d7fed82f5f

---
+++ docs/rust/opendal_core/layers/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["ConcurrentLimitLayer","DtraceLayer","HttpClientLayer","LoggingLayer","RetryLayer","SimulateLayer","TimeoutLayer"],"trait":["LoggingInterceptor","RetryInterceptor"]};
